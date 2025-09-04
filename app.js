fcd834aae393e83dd4731668ca5b3d53c51b6244
Mfayvor <mfavour918@gmail.com>
2025-09-04 08:32:43 -0500

    plus and minus buttons functional

--- app.js
+++ app.js
@@ -29,11 +29,18 @@ let shopItemsData = [{
   img: "images/img-2.jpg"
 }];
 
-let generateShop = () => {
+let basket = [
+//   {
+//   // id:"guufgdsf",
+//   // item: 1
+// }
+];
+
+let generateShop = ()=> {
   return (shop.innerHTML = shopItemsData.map((x)=>{
     let { id, name, price, desc, img } = x;
     return  `
-    <div class="item">
+    <div id=product-id-${id} class="item">
       <img width="220" src=${x.img} alt="">
       <div class="details">
         <h3>${x.name}</h3>
@@ -41,9 +48,9 @@ let generateShop = () => {
         <div class="price-quantity">
           <h2>$ ${x.price}</h2>
           <div class="buttons">
-            <i class="bi bi-dash-lg"></i>
-            <div class="quantity">0</div>
-            <i class="bi-plus-lg"></i>
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id=${id} class="quantity">0</div>
+            <i onclick="increment(${id})" class="bi-plus-lg"></i>
           </div>
         </div>
       </div>
@@ -53,3 +60,44 @@ let generateShop = () => {
 };
 
 generateShop();
+
+let increment = (id)=> {
+  let selectedItem = id;
+  let search = basket.find((x)=> x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+    id: selectedItem.id,
+    item: 1,
+  });
+  } 
+  else {
+    search.item += 1;
+  }
+
+
+  // console.log(basket);
+  update(selectedItem.id);
+};
+let decrement = (id)=> {
+  let selectedItem = id;
+  let search = basket.find((x)=> x.id === selectedItem.id);
+
+  if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  // console.log(basket);
+  update(selectedItem.id);
+};
+let update = (id) => {
+  let search = basket.find((x) =>x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation()
+};
+
+let calculation = ()=>{
+  console.log("calculation function is running")
+};
