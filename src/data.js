e270dc7965f94161ab3ff2b2ee436d5c2d955150
Mfayvor <mfavour918@gmail.com>
2025-09-09 08:15:12 -0500

    fin

--- src/data.js
+++ src/data.js
@@ -3,26 +3,57 @@ let shopItemsData = [{
   name: "Casual Shirt",
   price: 45,
   desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-  img: "images/img-1.jpg"
+  img: "images/PHOTO-2025-09-09-12-43-01.jpg"
 },
 {
   id: "frufuerh",
   name: "Office Shirt",
   price: 100,
   desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-  img: "images/img-2.jpg"
+  img: "images/PHOTO-2025-09-09-12-43-00-2.jpg"
 },
 {
   id: "ehduehfdfn",
   name: "T-Shirt",
   price: 25,
   desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-  img: "images/img-3.jpg"
+  img: "images/PHOTO-2025-09-09-12-43-05.jpg"
 },
 {
   id: "gefrufrhf",
   name: "Men's Suit",
   price: 300,
   desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-  img: "images/img-4.jpg"
+  img: "images/PHOTO-2025-09-09-12-43-03.jpg"
+},
+{
+  id: "rfherferhfeiel",
+  name: "Canvas",
+  price: 15,
+  desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  img: "images/PHOTO-2025-09-09-12-43-02.jpg"
+},
+{
+  id: "duggfdsufg",
+  name: "Shoes",
+  price: 20,
+  img: "images/PHOTO-2025-09-09-12-43-00.jpg"
+},
+{
+  id: "vefwifug",
+  name: "Suit",
+  price: 250,
+  img: "images/PHOTO-2025-09-09-12-43-01-2.jpg"
+},
+{
+  id: "egewufguf",
+  name: "Jean Jacket",
+  price: 50,
+  img: "images/PHOTO-2025-09-09-12-43-06.jpg"
+},
+{
+  id: "fbbdsbhc",
+  name: "Ties",
+  price: 5,
+  img: "images/PHOTO-2025-09-09-12-43-01-3.jpg"
 }];
